a7d1fe4b4d9f686af93a6aa0be6ca8111dfcef5b
pranta-barua007 <prantabarua247@gmail.com>
2022-01-02 21:48:27 +0600

    added horizontal scroll hook

--- components/card/section-cards.component.jsx
+++ components/card/section-cards.component.jsx
@@ -1,14 +1,17 @@
 import Link from "next/link";
 import Card from "./card.component";
 import clsx from "classnames";
+import { useHorizontalScroll } from "../../hooks/useHorizontalScroll";
 import styles from "./section-cards.module.css";
 
 const SectionCards = (props) => {
   const { title, videos = [], size, shouldWrap = false, shouldScale } = props;
+  const scrollRef = useHorizontalScroll();
+
   return (
     <section className={styles.container}>
       <h2 className={styles.title}>{title}</h2>
-        <div className={clsx(styles.cardWrapper, shouldWrap && styles.wrap)}>
+        <div ref={scrollRef} className={clsx(styles.cardWrapper, shouldWrap && styles.wrap)}>
           {videos.map((video, idx) => {
             return (
               <Link key={idx} href={`/video/${video.id}`}>
